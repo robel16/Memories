f0c6d78ee746e53a65d5e62564e87ba7d48dc2a3
robel <robelmuluwork@gmail.com>
2023-09-15 23:30:53 +0300

    user stored in sanity

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,13 +1,39 @@
-// import sanityClient, { SanityClient } from '@sanity/client'
+// import sanityClient from '@sanity/client';
+// import imageUrlBuilder from '@sanity/image-url';
 
-// import 
-// export const client=SanityClient({
-//   projectId:'gj0h2px7',
-//   dataset:'production',
-//   apiVersion:'2021-10-21',
-//   usecdn:true,
-//   token:process.env.SANITY_PROJECT_TOKEN
+// export const client = sanityClient({
+//   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+//   dataset: 'production',
+//   apiVersion: '2023-05-0',
+//   useCdn: true,
+//   token: process.env.REACT_APP_SANITY_TOKEN,
+// });
 
-// })
-// const builder=ImageUrlBuilder(client);
-// export const urlFor=(source)=>builder.image(source)
+// const builder = imageUrlBuilder(client);
+
+// export const urlFor = source => builder.image(source);
+
+import { createClient } from '@sanity/client';
+
+export const client = createClient({
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: 'production',
+  apiVersion: '2022-02-01',
+  useCdn: true,
+  token: process.env.REACT_APP_SANITY_TOKEN,
+});
+
+// Implement the 'createIfNotExist' function
+export async function createIfNotExist(document) {
+  try {
+    const existingDoc = await client.getDocument(document._id);
+    if (!existingDoc) {
+      // The document does not exist, so create it
+      await client.create(document);
+    }
+    // Document already exists or was created
+    return existingDoc || document;
+  } catch (error) {
+    throw error;
+  }
+}
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,66 +1,72 @@
-import React from 'react'
-import GoogleLogin from 'react-google-login'
-import { googleProvider } from '../Context/AuthContext';
-import { json, useNavigate } from 'react-router-dom'
-import {FcGoogle} from 'react-icons/fc';
-import {GoogleButton} from 'react-google-button'
+import React from 'react';
+import { GoogleButton } from 'react-google-button';
 import shareVideo from '../assets/share.mp4';
-import logo from '../assets/logowhite.png'
+import logo from '../assets/logowhite.png';
 import { UserAuth } from '../Context/AuthContext';
-const Login=()=> {
+import { createIfNotExist, client } from '../client';
+import { useNavigate } from 'react-router-dom';
 
-  const {googleSignIn}=UserAuth();
-  const {user,logOut}=UserAuth();
- const handleGoogleSignIn=async()=>{
-  try {
-    await googleSignIn()
-  
-    localStorage.setItem('user',JSON.stringify(user))
-    const {displayName,photoURL,uid}=user
-    console.log("user name is ", user.displayName)
-    console.log("user name is ", user.photoURL)
-    const doc={
-      _id:uid,
-      _type:'user',
-      userName:displayName,
-      image:photoURL
+const Login = () => {
+  const navigate = useNavigate();
+  const { googleSignIn } = UserAuth();
+  const { user, logOut } = UserAuth();
+
+  const handleGoogleSignIn = async () => {
+    try {
+      await googleSignIn();
+
+      // Store user data in localStorage
+      localStorage.setItem('user', JSON.stringify(user));
+
+      const { displayName, photoURL, uid } = user;
+      const doc = {
+        _id: uid,
+        _type: 'user',
+        username: displayName,
+        image: photoURL,
+      };
+
+      // Check if the document exists and create it if it doesn't
+      await createIfNotExist(doc);
+
+      // Navigate to the desired page after successful login
+      navigate('/', { replace: true });
+    } catch (error) {
+      console.log(error);
     }
-  } catch ( error) {
-    console.log(error)
-  }
- }
- const handlesignout=async()=>{
-try {
-  await logOut()
-} catch (error) {
-  console.log(error)
-}
- }
+  };
+
+  const handlesignout = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className='flex justify-center items-center flex-col h-screen'>
-      <div className='relative w-full h-full'>
-<video
-src={shareVideo}
-type="video/mp4"
-loop
-controls={false}
-muted
-autoPlay
-className='w-full h-full object-cover'
-/>
-<div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
-  <div className='p-5'>
-    <img src={logo} width="130px" alt="logo" />
-  </div>
-  <div className='shadow-2xl'>
-{/* <GoogleButton onClick={handleGoogleSignIn}/> */}
-<GoogleButton onClick={handleGoogleSignIn}/>
-  </div>
-</div>
+    <div className="flex justify-center items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" alt="logo" />
+          </div>
+          <div className="shadow-2xl">
+            <GoogleButton onClick={handleGoogleSignIn} />
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
-
+export default Login;
