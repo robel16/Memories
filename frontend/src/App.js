3475666ff4a73e802bc3ba5f549b2ddc027d821a
robel <robelmuluwork@gmail.com>
2023-10-19 23:17:25 +0300

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./container/Home";
 import { AuthContextProvider } from "./Context/AuthContext";
 const App = () => {
   return (
-    <AuthContextProvider>
+    <StrictMode>
+      <AuthContextProvider>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
       </Routes>
     </AuthContextProvider>
+    </StrictMode>
+    
       
    
   );
