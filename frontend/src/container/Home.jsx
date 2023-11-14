b1fe9d6f4e6fecdcc7ab12e95161d8fe0c009cb5
robel16 <robelmuluwork@gmail.com>
2023-11-14 03:11:44 +0300

    minor changes in home js

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -24,24 +24,23 @@ const scrollref=useRef()
     });
   }, [userInfo]); // Add userInfo as a dependency
 
-  useEffect(() => {
-    if (scrollref.current) {
-      scrollref.current.ScrollTo(0, 0); // Check if scrollref is defined before accessing its methods
-    }
-  }, []);
+  useEffect(()=>{
+    scrollref.current.scrollTo(0,0)
+  },[])
 
   const handleToggleSidebar = () => {
     setToggleSidebar(!ToggleSidebar); // Toggle the value of ToggleSidebar
   };
 
   return (
+    <>
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out '>
       <div className='hidden md:flex h-screen flex-initial'>
         <Sidebar user={user && user} />
       </div>
       <div className='flex md:hidden flex-row'>
         <div className='p-2 w-full flex flex-row justify-between items-center shadow-md'>
-<HiMenu fontSize={40} className='cursor cursor-pointer' onClick={handleToggleSidebar} />
+<HiMenu fontSize={40} className=' cursor-pointer' onClick={handleToggleSidebar} />
         <Link to='/'>
           <img src={logo} alt='logo' className='w-28' />
         </Link>
@@ -49,8 +48,10 @@ const scrollref=useRef()
           <img src={user?.image} alt='logo' className='w-28' />
         </Link>
         </div>
+ 
+        </div>
     {ToggleSidebar && (
-          <div className='fixed w-4/5 bg-white h-full overflow-y-auto shadow-md z-10 animate-slide-in'>
+          <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
             <div className='absolute w-full flex justify-end items-center p-2'>
               <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={handleToggleSidebar} />
             </div>
@@ -60,15 +61,15 @@ const scrollref=useRef()
         </div>
         
     
-        <div className='pb-2 flex-1 h-screen overflow-y-auto ref={scrollref} '>
+        <div className='pb-2 flex-1 h-screen overflow-y-auto 'ref={scrollref} >
           <Routes>
             <Route path="/user-profile/:userId" element={<UserProfile/>}/>
              <Route path="/*" element={<Pins user={user && user }/>}/>
           </Routes>
-        </div>
+        
       </div>
    
-  );
+      </>);
 };
 
 export default Home;
